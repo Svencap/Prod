e5089ab4d1304a550c7810b174549ac11c8ab45b
Svencap <Ayzekmanduar@mail.ru>
2023-02-10 19:23:58 +0900

    refactoring

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,7 +1,8 @@
 import { Link } from "react-router-dom";
 import { classNames } from "shared/lib/classNames/classNames";
 import cls from './Navbar.module.scss';
-import { AppLink, AppLinkThemes } from "shared/AppLink/ui/AppLink";
+import { AppLink, AppLinkThemes } from "shared/ui/AppLink/ui/AppLink";
+import { ThemeSwitcher } from "shared/ui/ThemeSwitcher";
 
 interface NavbarProps {
   className?: string
@@ -9,7 +10,8 @@ interface NavbarProps {
 
 export const Navbar = ({ className }: NavbarProps) => {
   return (
-    <div className={classNames(cls.navbar)}>
+    <div className={classNames(cls.navbar, {}, [className])}>
+      <ThemeSwitcher /> 
       <div className={cls.links}>
       <AppLink to={"/"} theme={AppLinkThemes.SECONDARY} className={cls.mainLink}>Главная</AppLink>
       <AppLink to={"/about"} theme={AppLinkThemes.SECONDARY}>О сайте</AppLink>
