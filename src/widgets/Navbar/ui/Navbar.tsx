99274aa2f4bafdc314f10e7c81595d86fbf2b4d1
Svencap <Ayzekmanduar@mail.ru>
2023-02-20 02:46:24 +0900

    add themeSwitcher in Sidebar

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -2,7 +2,6 @@ import { Link } from "react-router-dom";
 import { classNames } from "shared/lib/classNames/classNames";
 import cls from './Navbar.module.scss';
 import { AppLink, AppLinkThemes } from "shared/ui/AppLink/ui/AppLink";
-import { ThemeSwitcher } from "shared/ui/ThemeSwitcher";
 
 interface NavbarProps {
   className?: string
@@ -11,7 +10,6 @@ interface NavbarProps {
 export const Navbar = ({ className }: NavbarProps) => {
   return (
     <div className={classNames(cls.navbar, {}, [className])}>
-      <ThemeSwitcher /> 
       <div className={cls.links}>
       <AppLink to={"/"} theme={AppLinkThemes.SECONDARY} className={cls.mainLink}>Главная</AppLink>
       <AppLink to={"/about"} theme={AppLinkThemes.SECONDARY}>О сайте</AppLink>
