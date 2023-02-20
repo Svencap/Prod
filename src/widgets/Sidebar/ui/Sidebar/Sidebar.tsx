d257144e5f52bf435cfc74e650a1e47f160bd559
Svencap <Ayzekmanduar@mail.ru>
2023-02-20 21:11:34 +0900

    add i18next

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { ThemeSwitcher } from 'shared/ui/ThemeSwitcher';
 import cls from './Sidebar.module.scss';
+import { LangSwitcher } from 'shared/ui/LangSwitcher/ui/LangSwitcher';
 
 
 interface SidebarProps {
@@ -18,7 +19,8 @@ export const Sidebar = ({ className }: SidebarProps) => {
     <div className={classNames(cls.Sidebar, { [cls.collapsed]: collapsed }, [className])}>
       <button onClick={onToggle}>Toggle</button>
       <div className={cls.switchers}>
-      <ThemeSwitcher /> 
+      <ThemeSwitcher />
+      <LangSwitcher className={cls.lang}/>
       </div>
     </div>
   );
