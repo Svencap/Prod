26381578060a8c20c0555e2380b0be545f607946
Svencap <Ayzekmanduar@mail.ru>
2023-02-21 23:05:25 +0900

    add eslint i18n plugin

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,17 +1,20 @@
-import { Link } from 'react-router-dom';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { AppLink, AppLinkThemes } from 'shared/ui/AppLink/ui/AppLink';
+import { useTranslation } from 'react-i18next';
 import cls from './Navbar.module.scss';
 
 interface NavbarProps {
   className?: string
 }
 
-export const Navbar = ({ className }: NavbarProps) => (
-    <div className={classNames(cls.navbar, {}, [className])}>
-        <div className={cls.links}>
-            <AppLink to="/" theme={AppLinkThemes.SECONDARY} className={cls.mainLink}>Главная</AppLink>
-            <AppLink to="/about" theme={AppLinkThemes.SECONDARY}>О сайте</AppLink>
+export const Navbar = ({ className }: NavbarProps) => {
+    const { t, i18n } = useTranslation();
+    return (
+        <div className={classNames(cls.navbar, {}, [className])}>
+            <div className={cls.links}>
+                <AppLink to="/" theme={AppLinkThemes.SECONDARY} className={cls.mainLink}>Главная</AppLink>
+                <AppLink to="/about" theme={AppLinkThemes.SECONDARY}>О сайте</AppLink>
+            </div>
         </div>
-    </div>
-);
+    );
+};
