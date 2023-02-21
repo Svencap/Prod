cf47d6fd6710b3debeabd9aad55cec3f07b02352
Svencap <Ayzekmanduar@mail.ru>
2023-02-22 02:01:57 +0900

    add babel (extract i18n)

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -18,6 +18,22 @@ export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
         ],
     };
 
+    const babelLoader = {
+        test: /\.(js|jsx|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+            loader: 'babel-loader',
+            options: {
+                presets: [
+                    ['@babel/preset-env', { targets: 'defaults' }],
+                ],
+                plugins: [
+                    ['i18next-extract', { locales: ['ru', 'en'], keyAsDefaultValue: true }],
+                ],
+            },
+        },
+    };
+
     const cssLoader = {
         test: /\.s[ac]ss$/i,
         use: [
@@ -43,5 +59,5 @@ export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
         exclude: /node_modules/,
     };
 
-    return [typescriptLoader, cssLoader, svgLoader, fileLoader];
+    return [babelLoader, typescriptLoader, cssLoader, svgLoader, fileLoader];
 }
