0c1216187ed20a70214fc289ae3303ffe0441c82
Svencap <ayzekmanduar@mail.ru>
2023-05-08 03:26:20 +0900

    added ComponentRender for unit tests

---
+++ src/shared/lib/tests/componentRender/ComponentRender.tsx
@@ -0,0 +1,19 @@
+import { ReactNode } from 'react';
+import i18ConfigForTests from 'shared/config/i18nConfig/configForTesting';
+import { I18nextProvider } from 'react-i18next';
+import { render } from '@testing-library/react';
+import { MemoryRouter } from 'react-router';
+
+export interface ComponentRenderOptions {
+  route?: string;
+}
+
+export const ComponentRender = (component: ReactNode, options: ComponentRenderOptions = {}) => {
+    const { route = '/' } = options;
+    return render(
+        <MemoryRouter initialEntries={[route]}>
+            <I18nextProvider i18n={i18ConfigForTests}>{component}</I18nextProvider>
+            ,
+        </MemoryRouter>,
+    );
+};
--- src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
@@ -3,14 +3,15 @@ import { Sidebar } from 'widgets/Sidebar/ui/Sidebar/Sidebar';
 import {
     renderWithTranslations,
 } from 'shared/lib/tests/renderWithTranslations/renderWithTranslations';
+import { ComponentRender } from 'shared/lib/tests/componentRender/ComponentRender';
 
 describe('Test Sidebar', () => {
     test('test render', () => {
-        renderWithTranslations(<Sidebar />);
+        ComponentRender(<Sidebar />);
         expect(screen.getByTestId('sidebar')).toBeInTheDocument();
     });
     test('test toggle sidebar', () => {
-        renderWithTranslations(<Sidebar />);
+        ComponentRender(<Sidebar />);
         const toggleBtn = screen.getByTestId('sidebar-toggle');
         expect(screen.getByTestId('sidebar')).toBeInTheDocument();
         fireEvent.click(toggleBtn);
